d8d9a626c7fe9c4d2ae6e3e3bca61da6b105d8a5
AdarshBantakal <163523007+AdarshBantakal@users.noreply.github.com>
2024-11-05 21:43:56 +0530

    Update M5.java

--- M5.java
+++ M5.java
@@ -11,10 +11,11 @@ package pkg1;
  */
 class Tools{
 	String s1="";
-	String s2="";
+	
 	int len1=0;
 	public String reverse(String s1) {
 		len1=s1.length();
+		String s2="";
 		for(int i=0;i<len1;i++) {
 			s2=s2+s1.substring(len1-(i+1),len1-i);
 		}
