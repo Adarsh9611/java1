d69c5e1313a403c70f9405ba9e4d0e543727363c
Adarsh9611 <adarsh.23cs003@sode-edu.in>
2024-11-05 15:40:09 +0530

    Add files via upload

---
+++ M1.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg1;
+
+/**
+ *
+ * @author smv
+ */
+public class M1 {
+            public static void main(String[] args){
+                System.out.println("even numbers");
+                for(int i=0;i<=100;i++){
+                       if(i%2==0){
+                    System.out.println(i);
+                }
+                }
+                 System.out.println("odd numbers");
+                     for(int i=0;i<=100;i++){
+                    if(i%2!=0)
+                    {
+                        System.out.println(i);
+                    }
+            }
+                      System.out.println("multiple of 3");
+                       for(int i=3;i<=100;i++){
+                    if(i%3==0)
+                    {
+                        System.out.println(i);
+                    }
+                     System.out.println("multiple of 4");
+                       for(i=4;i<=100;i++){
+                    if(i%4==0)
+                    {
+                        System.out.println(i);
+                    }
+}
+}
+}
+}
