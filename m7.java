2546593378234989596d44078e6ceaec2cb28a47
AdarshBantakal <163523007+AdarshBantakal@users.noreply.github.com>
2024-11-05 21:45:02 +0530

    Update m7.java

--- m7.java
+++ m7.java
@@ -6,10 +6,11 @@
 package pkg1;
 class Tools2{
 	String s1="";
-	String s2="";
+	
 	int len1=0;
 	public String reverse(String s1) {
 		len1=s1.length();
+		String s2="";
 		for(int i=0;i<len1;i++) {
 			s2=s2+s1.substring(len1-(i+1),len1-i);
 		}
@@ -23,7 +24,7 @@ class Tools2{
  */
 public class m7 {
     public static void main(String[] args){
-        Tools2 t1=new Tools2();
+        Tools t1=new Tools();
         System.out.println(t1.reverse("Madan"));
     }
     
